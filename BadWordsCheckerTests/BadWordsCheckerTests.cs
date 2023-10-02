62f60c3a33eabb501c3f13fe882dbd0546d0112e
unBeam <beamdev@yandex.ru>
2023-10-03 01:20:06 +0500

    Добавьте файлы проекта.

---
+++ BadWordsCheckerTests/BadWordsCheckerTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Homework_03102023;
+using System.Collections.Generic;
+
+namespace BadWordsCheckerTests
+{
+    [TestClass]
+    public class BadWordsCheckerTests
+    {
+        /// <summary>
+        /// простая проверка замены
+        /// </summary>
+        [TestMethod]
+        public void Censore_StringWith1badword_Returned3Star()
+        {
+            // Arrange
+            string str = "редиска ананас";
+            string expected = "*** ананас";
+            //Act
+            string action = BadWordsChecker.Censore(str);
+            //Assert
+            Assert.AreEqual(expected, action);
+        }
+        /// <summary>
+        /// двойная проверка замены
+        /// </summary>
+        [TestMethod]
+        public void Censore_StringWith2badword_Returned6Star()
+        {
+            // Arrange
+            string str = "редиска редиска";
+            string expected = "*** ***";
+            //Act
+            string action = BadWordsChecker.Censore(str);
+            //Assert
+            Assert.AreEqual(expected, action);
+        }
+        /// <summary>
+        /// пустая строка
+        /// </summary>
+        [TestMethod]
+        public void Censore_EmptryString_ReturnedEmptryString()
+        {
+            // Arrange
+            string str = "";
+            string expected = "";
+            //Act
+            string action = BadWordsChecker.Censore(str);
+            //Assert
+            Assert.AreEqual(expected, action);
+        }
+        [TestMethod]
+        /// <summary>
+        /// большая проверка
+        /// </summary>
+        public void Censore_TenRedisok_Returned30stars()
+        {
+            // Arrange
+            string str = "редиска редиска редиска редиска редиска редиска редиска редиска редиска редиска";
+            string expected = "*** *** *** *** *** *** *** *** *** ***";
+            //Act
+            string action = BadWordsChecker.Censore(str);
+            //Assert
+            Assert.AreEqual(expected, action);
+        }
+        /// <summary>
+        /// проверка регистра
+        /// </summary>
+        [TestMethod]
+        public void Censore_UpperRediska_Returned3Stars()
+        {
+            // Arrange
+            string str = "РЕДИСКА РУЛИТ";
+            string expected = "*** рулит";
+            //Act
+            string action = BadWordsChecker.Censore(str);
+            //Assert
+            Assert.AreEqual(expected, action);
+        }
+    }
+}
---
+++ Homework_03102023/BadWordsChecker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_03102023
+{
+    public class BadWordsChecker
+    {
+        /// <summary>
+        /// Заменяет во входной строке все неприличные слова на звездочки ***.
+        /// </summary>
+        /// <param name="textString"> качестве параметра передается строка, которая может быть пустой или содержать некоторый текст. В тексте может быть 0 или более слов "редиска", записанных в различном регистре.</param>
+        /// <returns>Возвращается отфильтрованный текст, в котором все слова редиска заменены на звездочки **</returns>
+            public static string Censore(string textString)
+            {
+                List<string> list = new List<string>();
+
+                textString = textString.ToLower();
+
+                list = textString.Split(' ').ToList();
+
+                var secondList = list.Select(x => x.Replace("редиска", "***")).ToList();
+
+                string stringWithoutBadWords = string.Join(" ", secondList.ToArray());
+
+                return stringWithoutBadWords;
+                
+            }
+    }
+}
